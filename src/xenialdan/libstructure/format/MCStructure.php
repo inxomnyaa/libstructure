aa2bdd6663e06c4d00681386963324a3cf09c4b5
XenialDan <thebigsmilexd@gmail.com>
2020-12-03 13:23:15 +0100

    Upgrade version, experimental blockentity support

--- src/xenialdan/libstructure/format/MCStructure.php
+++ src/xenialdan/libstructure/format/MCStructure.php
@@ -10,6 +10,8 @@ use OutOfRangeException;
 use pocketmine\block\Block;
 use pocketmine\block\BlockFactory;
 use pocketmine\block\BlockLegacyIds;
+use pocketmine\block\tile\Tile;
+use pocketmine\block\tile\TileFactory;
 use pocketmine\math\Vector3;
 use pocketmine\nbt\LittleEndianNbtSerializer;
 use pocketmine\nbt\NBT;
@@ -21,10 +23,11 @@ use pocketmine\Server;
 use pocketmine\utils\Filesystem;
 use pocketmine\utils\TextFormat;
 use pocketmine\world\format\PalettedBlockArray;
+use pocketmine\world\Position;
+use pocketmine\world\World;
 use UnexpectedValueException;
 use xenialdan\libstructure\exception\StructureFileException;
 use xenialdan\libstructure\exception\StructureFormatException;
-use xenialdan\MagicWE2\API;
 use xenialdan\MagicWE2\helper\BlockStatesEntry;
 use xenialdan\MagicWE2\helper\BlockStatesParser;
 
@@ -41,6 +44,7 @@ class MCStructure
 	public const TAG_PALETTE_DEFAULT = 'default';
 	public const TAG_PALETTE_BLOCK_PALETTE = 'block_palette';
 	public const TAG_PALETTE_BLOCK_POSITION_DATA = 'block_position_data';
+	public const TAG_PALETTE_BLOCK_ENTITY_DATA = 'block_entity_data';
 	public const EXTENSION_MCSTRUCTURE = '.mcstructure';
 	public const LAYER_BLOCKS = 0;
 	public const LAYER_LIQUIDS = 1;
@@ -51,9 +55,11 @@ class MCStructure
 	/** @var Vector3 */
 	private $size;
 	/** @var PalettedBlockArray[] */
-	private $blockLayers;
-	/** @var array */
-	private $entities;
+	private $blockLayers = [];
+	/** @var array|CompoundTag[] */
+	private $entities = [];
+	/** @var array|CompoundTag[] */
+	private $blockEntities = [];
 
 	/**
 	 * Parses a *.mcstructure file
@@ -131,6 +137,7 @@ class MCStructure
 		$paletteDefaultTag = $paletteCompound->getCompoundTag(self::TAG_PALETTE_DEFAULT);
 		$paletteBlocks = new PalettedBlockArray(BlockLegacyIds::AIR << 4);
 		$paletteLiquids = new PalettedBlockArray(BlockLegacyIds::AIR << 4);
+		$blockEntities = [];
 		/** @var BlockStatesEntry[] $paletteArray */
 		$paletteArray = [];
 		/** @var CompoundTag $blockCompoundTag */
@@ -139,12 +146,15 @@ class MCStructure
 			if ($blockState instanceof BlockStatesEntry) $paletteArray[$paletteIndex] = $blockState;
 			else print TextFormat::RED . $blockCompoundTag . " is not BlockStatesEntry";
 		}
+		/** @var CompoundTag $blockPositionData */
+		$blockPositionData = $paletteDefaultTag->getCompoundTag(self::TAG_PALETTE_BLOCK_POSITION_DATA);
 		//positions
+		var_dump($this->size);
 		$l = $this->size->getZ();
 		$h = $this->size->getY();
-		foreach (range(0, $this->size->getZ()) as $z) {
-			foreach (range(0, $this->size->getY()) as $y) {
-				foreach (range(0, $this->size->getX()) as $x) {
+		foreach (range(0, $this->size->getZ() - 1) as $z) {
+			foreach (range(0, $this->size->getY() - 1) as $y) {
+				foreach (range(0, $this->size->getX() - 1) as $x) {
 //					foreach ($blockIndicesList as $layerIndex => $layer) {
 //						$layer = reset($layer);//only default
 //						/** @var ListTag $layer */
@@ -154,6 +164,7 @@ class MCStructure
 //						}
 //					}
 					$offset = (int)(($x * $l * $h) + ($y * $l) + $z);
+					var_dump($offset);
 
 					//block layer
 					/** @var ListTag<IntTag> $tag */
@@ -185,11 +196,18 @@ class MCStructure
 							}
 						}
 					}
+					//nbt
+					if ($blockPositionData->hasTag((string)$offset)) {
+						/** @var CompoundTag<CompoundTag> $tag1 */
+						$tag1 = $blockPositionData->getCompoundTag((string)$offset);
+						$blockEntities[World::blockHash($x, $y, $z)] = $tag1->getCompoundTag(self::TAG_PALETTE_BLOCK_ENTITY_DATA);
+					}
 				}
 			}
 		}
 
 		$this->blockLayers = [$paletteBlocks, $paletteLiquids];
+		$this->blockEntities = $blockEntities;
 	}
 
 	/**
@@ -200,16 +218,32 @@ class MCStructure
 	public function blocks(int $layer = 0): Generator
 	{
 		if ($layer > count($this->blockLayers) || $layer < 0) throw new OutOfBoundsException('Layers must be in range 0...' . count($this->blockLayers));
-		for ($x = 0; $x <= $this->size->getX(); $x++) {
-			for ($y = 0; $y <= $this->size->getY(); $y++) {
-				for ($z = 0; $z <= $this->size->getZ(); $z++) {
+		for ($x = 0; $x < $this->size->getX(); $x++) {
+			for ($y = 0; $y < $this->size->getY(); $y++) {
+				for ($z = 0; $z < $this->size->getZ(); $z++) {
 					$fullState = $this->blockLayers[$layer]->get($x, $y, $z);
 					$block = BlockFactory::getInstance()->fromFullBlock($fullState);
-					[$block->getPos()->x,$block->getPos()->y,$block->getPos()->z] = [$x,$y,$z];
+					[$block->getPos()->x, $block->getPos()->y, $block->getPos()->z] = [$x, $y, $z];
 					yield $block;
 				}
 			}
 		}
+	}
+
+	public function translateBlockEntity(Position $position,Vector3 $origin): ?Tile
+	{//TODO offset
+		$hash = World::blockHash($position->getFloorX(), $position->getFloorY(), $position->getFloorZ());
+		$data = $this->blockEntities[$hash] ?? null;
+		if ($data === null) return null;
+		$tile = TileFactory::getInstance()->createFromData($position->getWorld(), $data);
+		if ($tile === null) return null;
+		var_dump($position);
+		$position = $tile->getPos()->asVector3();
+		var_dump($position);
+		$position = $position->subtractVector($this->structure_world_origin)->addVector($origin);
+		var_dump($position);
+		[$tile->getPos()->x, $tile->getPos()->y, $tile->getPos()->z] = [$position->x, $position->y, $position->z];
+		return $tile;
 	}
 
 }

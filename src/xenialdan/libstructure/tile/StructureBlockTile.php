4d4f178f806ea1f85b5d816f83e1fa05d37fb1a4
XenialDan <thebigsmilexd@gmail.com>
2021-10-03 19:49:12 +0200

    Make StructureBlockTile independent of MWE2

--- src/xenialdan/libstructure/tile/StructureBlockTile.php
+++ src/xenialdan/libstructure/tile/StructureBlockTile.php
@@ -14,7 +14,6 @@ use pocketmine\network\mcpe\protocol\types\StructureEditorData;
 use pocketmine\network\mcpe\protocol\types\StructureSettings;
 use pocketmine\world\World;
 use xenialdan\libstructure\window\StructureBlockInventory;
-use xenialdan\MagicWE2\session\data\Asset;
 
 class StructureBlockTile extends Spawnable implements Nameable, InventoryHolder
 {
@@ -80,6 +79,14 @@ class StructureBlockTile extends Spawnable implements Nameable, InventoryHolder
 	}
 
 	/**
+	 * @return Vector3
+	 */
+	public function getFromV3(): Vector3
+	{
+		return $this->fromV3;
+	}
+
+	/**
 	 * @param Vector3 $fromV3
 	 * @return StructureBlockTile
 	 */
@@ -90,6 +97,14 @@ class StructureBlockTile extends Spawnable implements Nameable, InventoryHolder
 	}
 
 	/**
+	 * @return Vector3
+	 */
+	public function getToV3(): Vector3
+	{
+		return $this->toV3;
+	}
+
+	/**
 	 * @param Vector3 $toV3
 	 * @return StructureBlockTile
 	 */
@@ -97,6 +112,14 @@ class StructureBlockTile extends Spawnable implements Nameable, InventoryHolder
 	{
 		$this->toV3 = $toV3;
 		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getTitle(): string
+	{
+		return $this->title ?? $this->getName();
 	}
 
 	/**
@@ -159,12 +182,12 @@ class StructureBlockTile extends Spawnable implements Nameable, InventoryHolder
 		return $this;
 	}
 
-	private function calculateOffset(Vector3 $holderV3): Vector3
+	public function calculateOffset(Vector3 $holderV3): Vector3
 	{
 		return $holderV3->subtractVector(Vector3::minComponents($this->fromV3, $this->toV3))->multiply(-1)->floor();
 	}
 
-	private function calculateSize(): Vector3
+	public function calculateSize(): Vector3
 	{
 		return $this->fromV3->subtractVector($this->toV3)->abs()->add(1, 1, 1);
 	}
@@ -187,12 +210,12 @@ class StructureBlockTile extends Spawnable implements Nameable, InventoryHolder
 		$nbt->setByte("rotation", 0);
 		$nbt->setLong("seed", 0);
 		$nbt->setByte("showBoundingBox", $this->showBoundingBox ? 1 : 0);
-		$nbt->setString("structureName", $this->title ?? $this->getName());
+		$nbt->setString("structureName", $this->getTitle());
 		$nbt->setInt("x", (int)$pos->x);
 		$nbt->setInt("xStructureOffset", (int)$offset->x);
 		$nbt->setInt("xStructureSize", (int)$size->x);
 		$nbt->setInt("y", (int)$pos->y);
-		$nbt->setInt("yStructureOffset", (int)$offset->y+1);//TODO remove +1 hack
+		$nbt->setInt("yStructureOffset", (int)$offset->y/*+1*/);//TODO remove +1 hack
 		$nbt->setInt("yStructureSize", (int)$size->y);
 		$nbt->setInt("z", (int)$pos->z);
 		$nbt->setInt("zStructureOffset", (int)$offset->z);
@@ -240,28 +263,28 @@ class StructureBlockTile extends Spawnable implements Nameable, InventoryHolder
 		return $this->mode;
 	}
 
-	public function getStructureEditorData(Asset $asset): StructureEditorData
+	public function getStructureEditorData(string $structureName,Vector3 $size, Vector3 $origin): StructureEditorData
 	{
 		$data = new StructureEditorData();
-		$data->structureName = $asset->displayname;
+		$data->structureName = $structureName;
 		$data->structureDataField = "";
 		$data->includePlayers = $this->isShowPlayers();
 		$data->showBoundingBox = $this->isShowBoundingBox();
 		$data->structureBlockType = $this->getMode();
-		$data->structureSettings = $this->getStructureSettings($asset);
+		$data->structureSettings = $this->getStructureSettings($size,$origin);
 		$data->structureRedstoneSaveMove = 0;
 		return $data;
 	}
 
-	private function getStructureSettings(Asset $asset): StructureSettings
+	private function getStructureSettings(Vector3 $size, Vector3 $origin): StructureSettings
 	{
 		$settings = new StructureSettings();
 		$settings->paletteName = "default";
 		$settings->ignoreEntities = !$this->isShowEntities();
 		$settings->ignoreBlocks = !$this->isShowBlocks();
-		$settings->structureSizeX = $asset->getSize()->getFloorX();
-		$settings->structureSizeY = $asset->getSize()->getFloorY();
-		$settings->structureSizeZ = $asset->getSize()->getFloorZ();
+		$settings->structureSizeX = $size->getFloorX();
+		$settings->structureSizeY = $size->getFloorY();
+		$settings->structureSizeZ = $size->getFloorZ();
 		$settings->structureOffsetX = 0;
 		$settings->structureOffsetY = 0;
 		$settings->structureOffsetZ = 0;//TODO position
@@ -270,7 +293,7 @@ class StructureBlockTile extends Spawnable implements Nameable, InventoryHolder
 		$settings->mirror = false;
 		$settings->integrityValue = 1.0;
 		$settings->integritySeed = 0;
-		$settings->pivot = $asset->getOrigin();
+		$settings->pivot = $origin;
 		return $settings;
 	}
 }
